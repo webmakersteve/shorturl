faa3b8e9e60e05628cebc1da1f9fff8a0f487e73
Stephen Parente <webmakersteve@gmail.com>
2014-08-26 13:55:52 -0400

    Got it converting ints to proper base

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var redis = require("redis"),
     client = redis.createClient(),
     express = require('express'),
-    app = express();
+    app = express(),
+    bodyparser = require('body-parser');
+
+app.use( bodyparser.urlencoded({ extended: true}) );
 
 client.on("error", function (err) {
     console.log("Error " + err);
@@ -29,3 +32,39 @@ app.get("/:shorturl*", function(req,res,next) {
         });        
     } else res.end();
 });
+
+app.post('/link', function(req,res,next) {
+    link = req.body.link || false;
+    if (!link) {
+        return res.end("Please supply a link");
+    }
+    var url = to66(323123);
+    client.set( url, link );
+    res.end("Link added: " + url + "\n");
+});
+
+
+function to66(num) {
+    // Base 66
+    var nums = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+    // Now that we have the nums we can just use powers to convert the base
+    var remainder = num,
+         base = nums.length,
+         index,
+	 temp,
+	 returnstr = "";
+    do {
+	for (var i = 0; Math.pow(base,i) < remainder; i++) {
+	    index = Math.pow(base,i);
+	}
+ 	
+	temp = remainder / index;
+	remainder = remainder % index;
+	
+	returnstr += nums[parseInt(temp)-1];
+
+    } while (remainder > 0);
+
+    return returnstr.split("").reverse().join("");
+
+}
