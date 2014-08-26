ba90357fcbc4efb86109123281c74bc4ae2ad01f
Stephen Parente <webmakersteve@gmail.com>
2014-08-26 12:40:23 -0400

    got redis going

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
 var redis = require("redis"),
-    client = redis.createClient();
-
-// if you'd like to select database 3, instead of 0 (default), call
-// client.select(3, function() { /* ... */ });
+    client = redis.createClient(),
+    express = require('express'),
+    app = express();
 
 client.on("error", function (err) {
     console.log("Error " + err);
 });
 
-client.set("string key", "string val", redis.print);
-client.hset("hash key", "hashtest 1", "some value", redis.print);
-client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
-client.hkeys("hash key", function (err, replies) {
-    console.log(replies.length + " replies:");
-    replies.forEach(function (reply, i) {
-        console.log("    " + i + ": " + reply);
-    });
-    client.quit();
+client.on("connect", function() {
+    app.listen(3000);
+});
+
+app.get('/', function(req,res,next) {
+    res.end("This is an index response");
+});
+
+app.get("/:shorturl*", function(req,res,next) {
+    var shorturl = req.params.shorturl || false;
+    if (shorturl) {
+	console.log(shorturl);
+        client.get(shorturl, function(err,data) {
+	if (err) return;
+	if (data) {
+	    console.log(data);
+	    res.end(data);
+	}
+        res.end();
+        });        
+    } else res.end();
 });
