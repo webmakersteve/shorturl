3d81400968f5519f788dfad440f7fdf7442633e1
Stephen Parente <webmakersteve@gmail.com>
2014-08-26 14:52:28 -0400

    Got it all working! Need to refactor now

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ var redis = require("redis"),
     client = redis.createClient(),
     express = require('express'),
     app = express(),
-    bodyparser = require('body-parser');
+    bodyparser = require('body-parser'),
+    expressLess = require('express-less'),
+    when = require('when');
 
 app.use( bodyparser.urlencoded({ extended: true}) );
+app.use(express.static( __dirname + '/public'));
+
+app.use('/css', expressLess(__dirname + '/less'));
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+var rootURL = "http://localhost:3000/";
+
 
 client.on("error", function (err) {
     console.log("Error " + err);
@@ -15,56 +26,77 @@ client.on("connect", function() {
 });
 
 app.get('/', function(req,res,next) {
-    res.end("This is an index response");
+    res.render('index', { });
 });
 
 app.get("/:shorturl*", function(req,res,next) {
     var shorturl = req.params.shorturl || false;
+
     if (shorturl) {
-	console.log(shorturl);
-        client.get(shorturl, function(err,data) {
-	if (err) return;
-	if (data) {
-	    console.log(data);
-	    res.end(data);
-	}
+      client.get("link_" + shorturl, function(err,url) {
+      	if (err) return;
+
+      	if (url) {
+      	    return res.redirect(url)
+      	}
+
         res.end();
-        });        
-    } else res.end();
+      });
+    } else {
+      res.end();
+      // Link does not exist
+    }
 });
 
 app.post('/link', function(req,res,next) {
-    link = req.body.link || false;
-    if (!link) {
-        return res.end("Please supply a link");
-    }
-    var url = to66(323123);
-    client.set( url, link );
-    res.end("Link added: " + url + "\n");
+  link = req.body.link || false;
+
+  if (!link) {
+    return res.end("Please supply a link");
+  }
+  getNewIndex().then(function(index) {
+    var url = to66(index);
+    client.set( "link_" + url, link );
+    res.render("link-added", {link: rootURL + url});
+  }).catch(function(e) {
+    res.render("error", {error: e});
+  });
+
 });
 
 
-function to66(num) {
-    // Base 66
-    var nums = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
-    // Now that we have the nums we can just use powers to convert the base
-    var remainder = num,
-         base = nums.length,
-         index,
-	 temp,
-	 returnstr = "";
-    do {
-	for (var i = 0; Math.pow(base,i) < remainder; i++) {
-	    index = Math.pow(base,i);
-	}
- 	
-	temp = remainder / index;
-	remainder = remainder % index;
-	
-	returnstr += nums[parseInt(temp)-1];
+function to66 (num) {
 
-    } while (remainder > 0);
+  // Base 66
+  var nums = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_";
+  // Now that we have the nums we can just use powers to convert the base
+  var remainder = num,
+      base = nums.length,
+      index,
+      temp,
+      returnstr = "";
 
-    return returnstr.split("").reverse().join("");
+  do {
+    for (var i = 0; Math.pow(base,i) < remainder; i++) {
+      index = Math.pow(base,i);
+    }
 
+    temp = remainder / index;
+    remainder = remainder % index;
+
+    returnstr += nums[parseInt(temp)-1];
+
+  } while (remainder > 0);
+
+  return returnstr.split("").reverse().join("");
+
+}
+
+function getNewIndex() {
+  return when.promise(function(resolve,reject,notify) {
+    client.send_command("eval", ["return #redis.call('keys', 'link_*')", "0"], function(err,result) {
+      if (err) return reject(err);
+      else resolve(result);
+    });
+  })
 }
